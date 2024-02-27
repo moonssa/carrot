2c93e08c5ea00e2d1247e3ec069c9008d6814f59
rebootedCoder2 <oborimoon@gmail.com>
2024-02-28 07:28:17 +0900

    portrait & landscape

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,8 +2,8 @@ import type { NextPage } from "next";
 
 const Home: NextPage = () => {
   return (
-    <div className="bg-slate-400 py-20 px-20 grid gap-10 min-h-screen">
-      <div className="bg-white sm:bg-red-400 md:bg-teal-400 lg:bg-indigo-400 xl:bg-yellow-400 p-6 rounded-2xl shadow-xl ">
+    <div className="bg-slate-400 xl:place-content-center py-20 px-20 grid gap-10 lg:grid-cols-2 xl:grid-cols-3 min-h-screen">
+      <div className="bg-white flex flex-col justify-between p-6 rounded-2xl shadow-xl ">
         <span className="font-semibold text-3xl">Select Item</span>
 
         <div className="flex justify-between">
@@ -23,8 +23,8 @@ const Home: NextPage = () => {
           Checkout
         </button>
       </div>
-      <div className="bg-white  overflow-hidden rounded-3xl shadow-xl group ">
-        <div className="bg-blue-500 p-6 pb-14">
+      <div className="bg-white overflow-hidden rounded-3xl shadow-xl  group ">
+        <div className=" portrait:bg-indigo-600 landscape:bg-teal-500 p-6 pb-14 xl:pb-52">
           <span className="text-white text-2xl">Profile</span>
         </div>
         <div className="rounded-3xl bg-white relative -top-5 p-6 ">
@@ -45,7 +45,7 @@ const Home: NextPage = () => {
           </div>
         </div>
       </div>
-      <div className="bg-white p-6 rounded-3xl shadow-xl ">
+      <div className="bg-white p-6 rounded-3xl shadow-xl lg:col-span-2 xl:col-span-1">
         <div className="flex mb-5 justify-between items-center">
           <span>⬅️</span>
           <div className="space-x-3">
