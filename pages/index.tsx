dc9a64a645347ca4375119d9db49482699c0c57e
rebootedCoder2 <oborimoon@gmail.com>
2024-02-29 08:35:45 +0900

    auth screen

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,17 @@
 import type { NextPage } from "next";
+import Enter from "./enter";
 
 const Home: NextPage = () => {
   return (
+    <div>
+      <Enter />
+    </div>
+  );
+};
+
+export default Home;
+
+/*
     <div className="bg-slate-400  xl:place-content-center py-20 px-20 grid gap-10 lg:grid-cols-2 xl:grid-cols-3 min-h-screen">
       <div className="bg-white dark:bg-black dark:text-white flex flex-col justify-between p-6 rounded-2xl shadow-xl ">
         <span className="font-semibold text-3xl">Select Item</span>
@@ -85,7 +95,7 @@ const Home: NextPage = () => {
     </div>
   );
 };
-
+*/
 /*
   return (
     
@@ -208,4 +218,3 @@ const Home: NextPage = () => {
   );
 };
 */
-export default Home;
