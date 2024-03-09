8b478251da87384472a5f9475c4cbac7178b9c3e
rebootedCoder2 <oborimoon@gmail.com>
2024-03-09 12:30:41 +0900

    styling  upload screen and html for community screen

--- pages/items/upload.tsx
+++ pages/items/upload.tsx
@@ -2,48 +2,63 @@ import type { NextPage } from "next";
 
 const Upload: NextPage = () => {
   return (
-    <div>
+    <div className="px-4 py-16">
       <div>
-        <div>
-          <label>
-            <svg
-              className="h-12 w-12"
-              stroke="currentColor"
-              fill="none"
-              viewBox="0 0 48 48"
-              aria-hidden="true"
-            >
-              <path
-                d="M28 8H12a4 4 0 00-4 4v20m32-12v8m0 0v8a4 4 0 01-4 4H12a4 4 0 01-4-4v-4m32-4l-3.172-3.172a4 4 0 00-5.656 0L28 28M8 32l9.172-9.172a4 4 0 015.656 0L28 28m0 0l4 4m4-24h8m-4-4v8m-12 4h.02"
-                strokeWidth={2}
-                strokeLinecap="round"
-                strokeLinejoin="round"
-              />
-            </svg>
+        <label className="w-full text-gray-600 hover:border-orange-500  hover:text-orange-500  flex items-center justify-center border-2 border-dashed border-gray-300 h-48 rounded-md">
+          <svg
+            className="h-12 w-12"
+            stroke="currentColor"
+            fill="none"
+            viewBox="0 0 48 48"
+            aria-hidden="true"
+          >
+            <path
+              d="M28 8H12a4 4 0 00-4 4v20m32-12v8m0 0v8a4 4 0 01-4 4H12a4 4 0 01-4-4v-4m32-4l-3.172-3.172a4 4 0 00-5.656 0L28 28M8 32l9.172-9.172a4 4 0 015.656 0L28 28m0 0l4 4m4-24h8m-4-4v8m-12 4h.02"
+              strokeWidth={2}
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            />
+          </svg>
 
-            <input type="file" />
-          </label>
-        </div>
+          <input className="hidden" type="file" />
+        </label>
       </div>
-      <div>
-        <label>Price</label>
-        <div>
-          <div>
-            <span>$</span>
+      <div className="my-5">
+        <label
+          className="mb-1 text-sm font-medium text-gray-900"
+          htmlFor="price"
+        >
+          Price
+        </label>
+        <div className="rounded-md relative shadow-sm flex items-center">
+          <div className="absolute left-0 pl-3 flex items-center justify-center pointer-events-none">
+            <span className="text-gray-500 text-sm">$</span>
           </div>
-          <input type="text" placeholder="0.00" />
-          <div>
-            <span>USD</span>
+          <input
+            className="appearance-none w-full pl-7 px-3 py-2 border border-gray-300 rounded-md  shadow-sm placeholder-gray-400 focus:outline-none focus:ring-orange-500 focus:border-orange-500"
+            id="price"
+            type="text"
+            placeholder="0.00"
+          />
+          <div className="absolute right-0 pr-3 items-center pointer-events-none">
+            <span className="text-gray-500 text-sm">USD</span>
           </div>
         </div>
       </div>
-      <div>
-        <label>Description</label>
+      <div className="my-5">
+        <label className="mb-1 text-sm font-medium text-gray-900">
+          Description
+        </label>
         <div>
-          <textarea rows={4} />
+          <textarea
+            className="mt-1 shadow-sm w-full  focus:ring-orange-500 rounded-md border-gray-300 focus:border-orange-500"
+            rows={4}
+          />
         </div>
       </div>
-      <button>Upload product</button>
+      <button className="mt-2 w-full bg-orange-500 hover:bg-orange-600 text-white px-4 py-2 border border-transparent rounded-md shadow-sm text-sm font-medium focus:ring-2 focus:ring-offset-2 focus:ring-orange-500 focus:outline-none">
+        Upload product
+      </button>
     </div>
   );
 };
